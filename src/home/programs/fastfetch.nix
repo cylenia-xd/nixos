5cfddd4ec999a83add0cbefe702b893a2d7490df
cylenia <me@cylenia.dev>
2025-12-22 09:34:05 +1100

    fastfetch fix

--- src/home/programs/fastfetch.nix
+++ src/home/programs/fastfetch.nix
@@ -15,8 +15,7 @@
       display ={
         separator = " -> ";
         color = {
-          key = "magenta";
-          separator = "red";
+          separator = "#cba6f7";
         };
       };
       modules = [
@@ -30,55 +29,57 @@
         {
           type = "cpu";
           key = "  CPU";
+          keyColor = "#cba6f7";
         }
         {
           type = "gpu";
           key = "  GPU";
+          keyColor = "#cba6f7";
         }
         {
           type = "memory";
           key = "  RAM";
+          keyColor = "#cba6f7";
         }
         {
           type = "disk";
           key = "  DISK";
+          keyColor = "#cba6f7";
         }
         {
           type = "os";
           key = "  OS";
+          keyColor = "#cba6f7";
         }
         {
           type = "kernel";
           key = "  KRNL";
+          keyColor = "#cba6f7";
         }
         {
           type = "shell";
           key = "  SH";
+          keyColor = "#cba6f7";
         }
         {
           type = "wm";
           key = "  WM";
+          keyColor = "#cba6f7";
         }
         {
           type = "command";
           key = "  AGE";
+          keyColor = "#cba6f7";
           text = "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days";
         }
         {
           type = "uptime";
           key = "  UP";
+          keyColor = "#cba6f7";
         }
         {
           type = "custom";
           format = "{##cba6f7}└──────────────────────────────────────────────────────┘";
-        }
-        {
-          type = "break";
-        }
-        {
-          type = "colors";
-          symbol = "@";
-          key = " ";
         }
       ];
     };
