58237532d0bb87d42156ffca1b6b98a6d20a2e6d
cylenia <me@cylenia.dev>
2025-12-23 15:18:43 +1100

    move secrets

--- hosts/paul/system/programs/cloudflared.nix
+++ hosts/paul/system/programs/cloudflared.nix
@@ -1,10 +1,10 @@
 { config, pkgs, ... }: {
   services.cloudflared = {
     enable = true;
-    certificateFile = /root/.cloudflared/cert.pem;
+    certificateFile = /opt/tunnelsecrets/cert.pem;
     tunnels = {
       "a47e5b10-67d7-44c0-a486-2c63b09bcd62" = {
-        credentialsFile = /root/.cloudflared/a47e5b10-67d7-44c0-a486-2c63b09bcd62.json;
+        credentialsFile = /opt/tunnelsecrets/tunnel.json;
         default = "http://localhost:80";
       };
     };
