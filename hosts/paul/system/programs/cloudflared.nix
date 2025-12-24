474e133afe2525ce67e9fc58f899cca5603aa1b1
cylenia <me@cylenia.dev>
2025-12-24 15:46:02 +1100

    change

--- hosts/paul/system/programs/cloudflared.nix
+++ hosts/paul/system/programs/cloudflared.nix
@@ -1,10 +1,10 @@
 { config, pkgs, ... }: {
   services.cloudflared = {
     enable = true;
-    certificateFile = "${config.sops.secrets.tunnel.certificate.path}";
+    certificateFile = "${config.sops.secrets.tunnel_certificate.path}";
     tunnels = {
       "a47e5b10-67d7-44c0-a486-2c63b09bcd62" = {
-        credentialsFile = "${config.sops.secrets.tunnel.credentials.path}";
+        credentialsFile = "${config.sops.secrets.tunnel_credentials.path}";
         default = "http://localhost:80";
       };
     };
