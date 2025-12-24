b8929487cc6b280dca83bb9aa66ee450b4425ba7
cylenia <me@cylenia.dev>
2025-12-24 16:08:19 +1100

    ingress

--- hosts/paul/system/programs/cloudflared.nix
+++ hosts/paul/system/programs/cloudflared.nix
@@ -5,7 +5,10 @@
     tunnels = {
       "a47e5b10-67d7-44c0-a486-2c63b09bcd62" = {
         credentialsFile = "${config.sops.secrets.tunnel_credentials.path}";
-        default = "http://localhost:80";
+        ingress = {
+          "paul.cylenia.dev" = "http://localhost:80";
+        };
+        default = "http_status:404";
       };
     };
   };
