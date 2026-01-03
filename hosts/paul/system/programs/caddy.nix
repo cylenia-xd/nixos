c40e756c110054afc6c02493458d271e1d59c002
cylenia <me@cylenia.dev>
2026-01-03 17:25:51 +1100

    maybe im dumb

--- hosts/paul/system/programs/caddy.nix
+++ hosts/paul/system/programs/caddy.nix
@@ -1,15 +1,12 @@
 { config, pkgs, ... }: {
   services.caddy = {
     enable = true;
-    virtualHosts.bio = {
-      hostName = "127.0.0.1";
-      extraConfig = ''
-        :8068 {
-          root * /srv/bio
-          file_server
-        }
-      '';
-    };
+    extraConfig = ''
+      :8068 {
+        root * /srv/bio
+        file_server
+      }
+    '';
   };
 }
 
