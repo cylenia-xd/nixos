38a67b06d1515f18500e522590f675a5ca61523f
cylenia <me@cylenia.dev>
2026-01-05 18:48:03 +1100

    dns

--- hosts/trissa/system/networking.nix
+++ hosts/trissa/system/networking.nix
@@ -2,4 +2,10 @@
   networking.hostName = "trissa";
   networking.networkmanager.enable = true;
   networking.firewall.enable = false;
+  networking.nameservers = [
+    "9.9.9.9"
+    "149.112.112.112"
+    "2620:fe::fe"
+    "2620:fe::9"
+  ]
 }
