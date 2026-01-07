86b496a8dc449f19849f2113e632e27ac256c274
cylenia <me@cylenia.dev>
2026-01-08 07:58:34 +1100

    neomutt colors

--- hosts/trissa/home/programs/neomutt.nix
+++ hosts/trissa/home/programs/neomutt.nix
@@ -1,10 +1,165 @@
 { config, pkgs, ... }: {
   programs.neomutt = {
     enable = true;
-    sidebar = {
-      enable = true;
-      shortPath = true;
-      width = 22;
+    sidebar.enable = false;
+    settings = {
+      color = {
+        normal = {
+          fg = "default";
+          bg = "default";
+        };
+
+        indexNew = {
+          fg = "color2";
+          bg = "default";
+          pattern = "~N";
+        };
+
+        indexFlagged = {
+          fg = "color1";
+          bg = "default";
+          pattern = "~F";
+        };
+
+        indexTagged = {
+          fg = "color13";
+          bg = "default";
+          pattern = "~T";
+        };
+
+        indexDeleted = {
+          fg = "color1";
+          bg = "default";
+          pattern = "~D";
+        };
+
+        attachment = {
+          fg = "color5";
+          bg = "default";
+        };
+
+        signature = {
+          fg = "color8";
+          bg = "default";
+        };
+
+        search = {
+          fg = "color4";
+          bg = "default";
+        };
+
+        indicator = {
+          fg = "default";
+          bg = "color8";
+        };
+
+        error = {
+          fg = "color1";
+          bg = "default";
+        };
+
+        status = {
+          fg = "color15";
+          bg = "default";
+        };
+
+        tree = {
+          fg = "color15";
+          bg = "default";
+        };
+
+        tilde = {
+          fg = "color15";
+          bg = "default";
+        };
+
+        hdrdefault = {
+          fg = "color13";
+          bg = "default";
+        };
+
+        headerFrom = {
+          fg = "color13";
+          bg = "default";
+          pattern = "^From:";
+        };
+
+        headerSubject = {
+          fg = "color13";
+          bg = "default";
+          pattern = "^Subject:";
+        };
+
+        quoted = {
+          fg = "color15";
+          bg = "default";
+        };
+
+        quoted1 = {
+          fg = "color7";
+          bg = "default";
+        };
+
+        quoted2 = {
+          fg = "color8";
+          bg = "default";
+        };
+
+        quoted3 = {
+          fg = "color0";
+          bg = "default";
+        };
+
+        quoted4 = {
+          fg = "color0";
+          bg = "default";
+        };
+
+        quoted5 = {
+          fg = "color0";
+          bg = "default";
+        };
+
+        bodyEmail = {
+          fg = "color2";
+          bg = "default";
+          pattern = ''[\-\.+_a-zA-Z0-9]+@[\-\.a-zA-Z0-9]+'';
+        };
+
+        bodyUrl = {
+          fg = "color2";
+          bg = "default";
+          pattern = ''(https?|ftp)://[\-\.,/%~_:?&=\#a-zA-Z0-9]+'';
+        };
+
+        bodyBold = {
+          fg = "color4";
+          bg = "default";
+          pattern = ''(^|[[:space:]])\*[^[:space:]]+\*([[:space:]]|$)'';
+        };
+
+        bodyUnderline = {
+          fg = "color4";
+          bg = "default";
+          pattern = ''(^|[[:space:]])_[^[:space:]]+_([[:space:]]|$)'';
+        };
+
+        bodyItalic = {
+          fg = "color4";
+          bg = "default";
+          pattern = ''(^|[[:space:]])/[^[:space:]]+/([[:space:]]|$)'';
+        };
+
+        sidebarFlagged = {
+          fg = "color1";
+          bg = "default";
+        };
+
+        sidebarNew = {
+          fg = "color10";
+          bg = "default";
+        };
+      };
     };
   };
 
