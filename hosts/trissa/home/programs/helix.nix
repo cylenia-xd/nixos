fc329bf6ee9fcc3752e77aba331b7c48376ca07a
cylenia <me@cylenia.dev>
2026-01-08 22:45:33 +1100

    what

--- hosts/trissa/home/programs/helix.nix
+++ hosts/trissa/home/programs/helix.nix
@@ -6,6 +6,7 @@
     settings.theme = "catppuccin_mocha";
     languages.language = [{
       name = "markdown";
+      auto-format = true;
       formatter = {
         command = "dprint";
         args = [ "fmt" "--stdin" "md" "--config" "~/.config/dprint/dprint.json"];
