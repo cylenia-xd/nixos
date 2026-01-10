751cbd30ae52667f03ec79dee74f3b9e67d21309
cylenia <me@cylenia.dev>
2026-01-10 15:24:22 +1100

    systemd service to make the secret

--- hosts/paul/system/programs/mailserver.nix
+++ hosts/paul/system/programs/mailserver.nix
@@ -17,6 +17,43 @@
     };
   };
 
+  systemd.services.dns-rfc2136-conf = {
+    requiredBy = [
+      "acme-mail.cylenia.dev.service"
+      "bind.service"
+    ];
+    before = [
+      "acme-mail.cylenia.dev.service"
+      "bind.service"
+    ];
+    unitConfig = {
+      ConditionPathExists = "!/var/lib/secrets/dnskeys.conf";
+    };
+    serviceConfig = {
+      Type = "oneshot";
+      UMask = 77;
+    };
+    path = [ pkgs.bind ];
+    script = ''
+      mkdir -p /var/lib/secrets
+      chmod 755 /var/lib/secrets
+      tsig-keygen rfc2136key.mail.cylenia.dev > /var/lib/secrets/dnskeys.conf
+      chown named:root /var/lib/secrets/dnskeys.conf
+      chmod 400 /var/lib/secrets/dnskeys.conf
+
+      # extract secret value from the dnskeys.conf
+      while read x y; do if [ "$x" = "secret" ]; then secret="''${y:1:''${#y}-3}"; fi; done < /var/lib/secrets/dnskeys.conf
+
+      cat > /var/lib/secrets/certs.secret << EOF
+      RFC2136_NAMESERVER='127.0.0.1:53'
+      RFC2136_TSIG_ALGORITHM='hmac-sha256.'
+      RFC2136_TSIG_KEY='rfc2136key.mail.cylenia.dev'
+      RFC2136_TSIG_SECRET='$secret'
+      EOF
+      chmod 400 /var/lib/secrets/certs.secret
+    '';
+  };
+
   mailserver = {
     enable = true;
     stateVersion = 3;
