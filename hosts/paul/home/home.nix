7216b27ff7d9c6ef48621c1fd451899be060fd04
cylenia <me@cylenia.dev>
2026-01-13 16:14:08 +1100

    stuff

--- hosts/paul/home/home.nix
+++ hosts/paul/home/home.nix
@@ -1,26 +1,9 @@
-{ config, pkgs, inputs, ... }: {
-  imports = [
-    ./programs/git.nix
-  ];
-
+{ pkgs, ... }: {
   home.packages = with pkgs; [
     git
     helix
     fastfetch
   ];
-
-  xdg.userDirs = {
-    enable = true;
-    createDirectories = true;
-    desktop = "desktop";
-    documents = "documents";
-    download = "downloads";
-    music = "music";
-    pictures = "pictures";
-    publicShare = "public";
-    templates = "templates";
-    videos = "videos";
-  };
 
   # don't change this!
   home.stateVersion = "25.11";
