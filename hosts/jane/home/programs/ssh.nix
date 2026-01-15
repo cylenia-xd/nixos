b0f760e01bb05cb3bf649b3f5095db653c529cdc
cylenia <me@cylenia.dev>
2026-01-15 19:08:10 +1100

    im stoopid

--- hosts/jane/home/programs/ssh.nix
+++ hosts/jane/home/programs/ssh.nix
@@ -7,13 +7,13 @@
         hostname = "192.227.173.19";
         port = 22;
         user = "root";
-        sendEnv = [ "TERM=xterm-256color" ];
+        setEnv.TERM = "xterm-256color";
       };
       paul = {
         hostname = "192.168.86.50";
         port = 22;
         user = "cylenia";
-        sendEnv = [ "TERM=xterm-256color" ];
+        setEnv.TERM = "xterm-256color";
       };
     };
   };
