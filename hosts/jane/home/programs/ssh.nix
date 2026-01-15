543bc770a75209cd31aa17c870f09b5625b2afb7
cylenia <me@cylenia.dev>
2026-01-15 19:03:41 +1100

    try using matchBlocks because its more nixy

--- hosts/jane/home/programs/ssh.nix
+++ hosts/jane/home/programs/ssh.nix
@@ -2,18 +2,19 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    extraConfig = ''
-      Host greg
-        Hostname 192.227.173.19
-        Port 22
-        User root
-        SetEnv TERM=xterm-256color
-
-      Host paul
-        Hostname 192.168.86.50
-        Port 22
-        User cylenia
-        SetEnv TERM=xterm-256color
-    '';
+    programs.ssh.matchBlocks = {
+      greg = {
+        hostname = "192.227.173.19";
+        port = 22;
+        user = "root";
+        sendEnv = [ "TERM=xterm-256color" ];
+      };
+      paul = {
+        hostname = "192.168.86.50";
+        port = 22;
+        user = "cylenia";
+        sendEnv = [ "TERM=xterm-256color" ];
+      };
+    };
   };
 }
