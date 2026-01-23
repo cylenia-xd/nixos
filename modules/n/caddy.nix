87cca53ae933977f9070a4ee299f331965f26abe
cylenia <me@cylenia.dev>
2026-01-24 10:02:32 +1100

    caddy: remove webhooks for now, need to rethink my impl

--- modules/n/caddy.nix
+++ modules/n/caddy.nix
@@ -1,22 +1,9 @@
 { pkgs, ... }: {
   services.caddy = {
     enable = true;
-    package = pkgs.caddy.withPlugins {
-      plugins = [ "github.com/greenpau/caddy-git@v1.0.9" ];
-      hash = "sha256-nh+Oq0M3xjRVlBH/QtYtbQ126CUSzQY5QvCuSrTKvlg=";
+    package = pkgs.caddy;
     };
     extraConfig = ''
-      :8067 {
-        git /github/cylenia-xd/bio {
-          path /srv/bio
-          then sh -c "cd /srv/bio && npm run compile"
-          hook /webhooks
-          hook_type github
-          clone_args --recursive
-          pull_args --recurse-submodules
-        }
-      }      
-
       :8068 {
         root * /srv/bio/_site
         file_server
@@ -28,7 +15,4 @@
       }
     '';
   };
-
-  systemd.services.caddy.path = [ pkgs.nodejs ];
 }
-
