fd361c1cf0c52844c285145c68f41f42f11cc403
cylenia <me@cylenia.dev>
2026-01-24 08:43:33 +1100

    caddy: fix hash

--- modules/n/caddy.nix
+++ modules/n/caddy.nix
@@ -3,6 +3,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/WingLim/caddy-webhook@v1.0.8" ];
+      hash = "sha256-oMmmKK4HkkNCUQVfMb7FImtkKmf9/oQ51h577W5WTEg=";
     };
     extraConfig = ''
       :8067 {
