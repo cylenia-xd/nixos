c32641a4d6892825b790e27481681e7d8ee8978d
cylenia <me@cylenia.dev>
2026-01-24 08:07:40 +1100

    caddy: update site by webhook

--- modules/n/caddy.nix
+++ modules/n/caddy.nix
@@ -1,7 +1,17 @@
-{ ... }: {
+{ pkgs, ... }: {
   services.caddy = {
     enable = true;
+    package = pkgs.xcaddy;
     extraConfig = ''
+      :8067 {
+        webhook {
+          repo https://github.com/cylenia/bio.git
+          path /srv/bio
+          branch master
+          command nix-shell --command npm run compile
+        }
+      }
+      
       :8068 {
         root * /srv/bio/_site
         file_server
