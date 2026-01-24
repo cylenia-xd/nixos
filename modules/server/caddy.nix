19c02b7aa3d0c1a0e55dda9e8638e55a2f7c63cc
cylenia <me@cylenia.dev>
2026-01-25 09:46:20 +1100

    declarative caddy

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -1,14 +1,27 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  bioSite = pkgs.stdenv.mkDerivation {
+    name = "bio-site";
+    src = fetchGit {
+      url = "https://github.com/cylenia-xd/bio.git";
+      ref = "master";
+    };
+    buildInputs = [ pkgs.nodejs ];
+    buildPhase = ''
+      npx @11ty/eleventy
+    '';
+    installPhase = ''
+      mkdir -p $out
+      cp -r _site/* $out/
+    '';
+  };
+in {
   services.caddy = {
     enable = true;
     package = pkgs.caddy;
     virtualHosts = {
       ":8068".extraConfig = ''
-        root * /srv/bio/_site
-        file_server
-      '';
-      ":8071".extraConfig = ''
-        root * /srv/countdown
+        root * ${bioSite}
         file_server
       '';
     };
