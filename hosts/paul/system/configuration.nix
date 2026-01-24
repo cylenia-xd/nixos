4aa6c78db7d5bb0fb805dbb619eb5f9f44266347
cylenia <me@cylenia.dev>
2026-01-24 17:20:33 +1100

    hosts/paul: declarative users

--- hosts/paul/system/configuration.nix
+++ hosts/paul/system/configuration.nix
@@ -10,10 +10,18 @@
     "${inputs.self}/modules/n/immich.nix"
   ];
 
-  users.users.cylenia = {
-    isNormalUser = true;
-    description = "Cylenia";
-    extraGroups = [ "networkmanager" "wheel" ];
+  users = {
+    mutableUsers = false;
+    users.cylenia = {
+      isNormalUser = true;
+      description = "Cylenia";
+      shell = pkgs.bash;
+      extraGroups = [ "networkmanager" "wheel" ];
+      hashedPassword = "$y$j9T$gej56YvgGE.K/x4UOF/mZ1$W7EQoyuqICLE8uV1oIxM.cI180PXT9xERVXMzDdtAV0";
+    };
+    users.root = {
+      hashedPassword = "$y$j9T$gej56YvgGE.K/x4UOF/mZ1$W7EQoyuqICLE8uV1oIxM.cI180PXT9xERVXMzDdtAV0";
+    };
   };
 
   boot.loader.systemd-boot.enable = true;
