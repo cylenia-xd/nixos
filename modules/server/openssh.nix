ab07827665f45714f267bc0862b0726549aeba9e
cylenia <me@cylenia.dev>
2026-01-25 12:43:00 +1100

    fix ssh settings for paul

--- modules/server/openssh.nix
+++ modules/server/openssh.nix
@@ -1,5 +1,9 @@
 {  ... }: {
   services.openssh = {
     enable = true;
+    authorizedKeysInHomedir = false;
   };
+
+  users.users.cylenia.openssh.authorizedKeys.keys =
+    [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE1XHWN+HMREOhta8Ltz9GpmqldZVDToXLi4kIftvk0B me@cylenia.dev" ];
 }
