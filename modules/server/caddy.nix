c71f84634e529cc4af70607495c3aeb5acf03147
cylenia <me@cylenia.dev>
2026-01-25 12:14:29 +1100

    fix hash

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -4,6 +4,7 @@ let
     owner = "cylenia-xd";
     repo = "bio";
     rev = "7f04f2f558a3220cc6ee092efa43fced172df5ae";
+    hash = "sha256-CdmfDI7coV+ySQg2MPBUqzo9HV+6Cv68Q6kYZHatf6s=";
   }) {};
 in {
   services.caddy = {
