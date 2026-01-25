b11aa0f8edcad7832124de2bcc2bda836e4a1599
cylenia <me@cylenia.dev>
2026-01-25 15:44:46 +1100

    bru

--- modules/server/flix.nix
+++ modules/server/flix.nix
@@ -3,7 +3,7 @@
   services.prowlarr = {
     enable = true;
     settings = {
-      server.urlbase = "http://192.168.86.50:7067";
+      server.urlbase = "";
       server.bindaddress = "*";
       server.port = 7067;
       server.enablessl = false;
