6dde692cbe53c34e1f07164fee4dc12bc48e2959
cylenia <me@cylenia.dev>
2026-01-28 10:12:22 +1100

    allow root

--- modules/server/openssh.nix
+++ modules/server/openssh.nix
@@ -6,4 +6,7 @@
 
   users.users.cylenia.openssh.authorizedKeys.keys =
     [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE1XHWN+HMREOhta8Ltz9GpmqldZVDToXLi4kIftvk0B me@cylenia.dev" ];
+
+  users.users.root.openssh.authorizedKeys.keys =
+    [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE1XHWN+HMREOhta8Ltz9GpmqldZVDToXLi4kIftvk0B me@cylenia.dev" ];
 }
