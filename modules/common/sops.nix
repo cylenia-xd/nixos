181660a149aa8247b9e5b56fddc38796eeed8b11
cylenia <me@cylenia.dev>
2026-02-04 19:24:52 +1100

    whatever

--- modules/common/sops.nix
+++ modules/common/sops.nix
@@ -1,12 +1,11 @@
-{ lib, config, ... }: {
+{ ... }: {
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/cylenia/.config/sops/age/keys.txt";
-  sops.secrets = (lib.mkIf (config.networking.hostName == "paul") {
+  sops.secrets = {
     pangolin_env = {};
     vaultwarden_env = {};
     mistral_api_key.owner = "librechat";
-  }) // (lib.mkIf (config.networking.hostName == "jane") {
     email_password.owner = "cylenia";
-  });
+  };
 }
