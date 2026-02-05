d24eb9d405fa6de11b85e4fc3cb03b63a114bbc0
cylenia <me@cylenia.dev>
2026-02-05 12:11:41 +1100

    hashes

--- modules/desktop/gaming.nix
+++ modules/desktop/gaming.nix
@@ -24,11 +24,13 @@
             enable = true;
             file = pkgs.fetchurl {
               url = "https://cdn.modrinth.com/data/1KVo5zza/versions/yqCCvD11/Fabulously.Optimized-v12.0.0.mrpack";
+              hash = "sha256-zantYaX/i3qYaTArMd9NYNYINON1IxMHJdFL5A5Y7NI=";
             };
           };
           files = {
             "mods/ias.jar".source = pkgs.fetchurl {
               url = "https://cdn.modrinth.com/data/cudtvDnd/versions/oSzaukGT/IAS-9.0.6-alpha.2%2B1.21.11-fabric.jar";
+              hash = "sha256-//1prgwr2wIdXFK448KF5iFl2cJWMwFuFyC1Bo058SI=";
             };
           };
         };
